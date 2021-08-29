3669cb6d87a2845bce0c048cbbe2cd87d82d9883
LukeBlueLOx <86468152+LukeBlueLOx@users.noreply.github.com>
2021-08-29 17:15:29 +0200

    Add files via upload

--- SARS-CoV-2_Poland - update 3 tab +1 day in The 7-day average sheet.py
+++ SARS-CoV-2_Poland - update 3 tab +1 day in The 7-day average sheet.py
@@ -21,7 +21,7 @@ result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="Średnia 7 dni - Zakres 2 tygodnie!B3").execute()
 values = result.get('values', [])
 
-RUN1 = [["={ 'Średnia 7 dni SARS-CoV-2'!H154 }", "={ 'Średnia 7 dni SARS-CoV-2'!I154 }"]]
+RUN1 = [["={ 'Średnia 7 dni SARS-CoV-2'!H137 }", "={ 'Średnia 7 dni SARS-CoV-2'!I137 }"]]
 
 request1 = service.spreadsheets().values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                                  range="Średnia 7 dni - Zakres 2 tygodnie!B3", valueInputOption="USER_ENTERED", body={"values":RUN1}).execute()
@@ -31,7 +31,7 @@ result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="Średnia 7 dni - Zakres 4 tygodnie!B3").execute()
 values = result.get('values', [])
 
-RUN2 = [["={ 'Średnia 7 dni SARS-CoV-2'!H140 }", "={ 'Średnia 7 dni SARS-CoV-2'!I140 }"]]
+RUN2 = [["={ 'Średnia 7 dni SARS-CoV-2'!H122 }", "={ 'Średnia 7 dni SARS-CoV-2'!I122 }"]]
 
 request2 = service.spreadsheets().values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                                  range="Średnia 7 dni - Zakres 4 tygodnie!B3", valueInputOption="USER_ENTERED", body={"values":RUN2}).execute()
@@ -41,9 +41,36 @@ result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="Prognoza liczby zgonów - 21 dni do przodu!B3").execute()
 values = result.get('values', [])
 
-RUN3 = [["={ 'Średnia 7 dni SARS-CoV-2'!O147 }", "={ 'Średnia 7 dni SARS-CoV-2'!P147 }"]]
+RUN3 = [["={ 'Średnia 7 dni SARS-CoV-2'!O129 }", "={ 'Średnia 7 dni SARS-CoV-2'!P129 }"]]
 
 request3 = service.spreadsheets().values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                                  range="Prognoza liczby zgonów - 21 dni do przodu!B3", valueInputOption="USER_ENTERED", body={"values":RUN3}).execute()                                                
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range="Karkonoski!B2").execute()
+values = result.get('values', [])
 
-print(request1, request2, request2)
+RUN4 = [["={ 'Średnia 7 dniowa / 100000'!I2 }", "={ 'Średnia 7 dniowa / 100000'!K2 }"]]
+
+request4 = service.spreadsheets().values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                                 range="Karkonoski!B2", valueInputOption="USER_ENTERED", body={"values":RUN4}).execute()
+
+
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range="Jelenia Góra!B2").execute()
+values = result.get('values', [])
+
+RUN5 = [["={ 'Średnia 7 dniowa / 100000'!T2 }", "={ 'Średnia 7 dniowa / 100000'!V2 }"]]
+
+request5 = service.spreadsheets().values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                                 range="Jelenia Góra!B2", valueInputOption="USER_ENTERED", body={"values":RUN5}).execute()
+
+
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range="Dolnośląskie!B2").execute()
+values = result.get('values', [])
+
+RUN6 = [["={ 'Średnia 7 dniowa / 100000'!AE2 }", "={ 'Średnia 7 dniowa / 100000'!AG2 }"]]
+
+request6 = service.spreadsheets().values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                                 range="Dolnośląskie!B23", valueInputOption="USER_ENTERED", body={"values":RUN6}).execute()
+print(request1, request2, request3, request4, request5, request6)
