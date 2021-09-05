f3f3e7de017b5d8071e562ca94845a7515c1ea35
LukeBlueLOx <86468152+LukeBlueLOx@users.noreply.github.com>
2021-09-05 20:34:31 +0200

    Update startupscript.sh

--- startupscript.sh
+++ startupscript.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/python3
 
-python3 /home/luke_blue_lox/clear.py
+python3 /home/luke_blue_lox/clear7tab.py
 
 python3 /home/luke_blue_lox//update.py
+
+python3 /home/luke_blue_lox//repeat.py
+
+python3 /home/luke_blue_lox/clear7days
+
+python3 /home/luke_blue_lox/append7days.py
+
+python3 /home/luke_blue_lox/append7days_100000.py
