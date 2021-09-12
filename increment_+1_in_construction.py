8f0e61dffd7e26730ac073c66357d8fc237547aa
LukeBlueLOx <86468152+LukeBlueLOx@users.noreply.github.com>
2021-09-12 12:38:12 +0200

    Add files via upload

--- increment_+1_in_construction.py
+++ increment_+1_in_construction.py
@@ -1,5 +1,7 @@
-f = open ("/home/luke_green_player/Downloads/Repeat/update.py", "a+")
-f.write ("123")
-f.close
+f = open ("/home/luke_green_player/Downloads/Repeat/test.py", "r+")
+f.write ('''from googleapiclient.discovery import build\nfrom google.oauth2 import service_account\n\nSERVICE_ACCOUNT_FILE = '/home/luke_green_player/Downloads/Repeat/sars-cov-2-poland.json'\nSCOPES = ['https://www.googleapis.com/auth/spreadsheets']\n\nRUN1 = [['=IMPORTRANGE("https://docs.google.com/spreadsheets/d/1ys-rIwopVtp_PDzr4tV_ycF1z1Vfug54BJ0IUVx1SNs","Średnia 7 dni SARS-CoV-2!H168:H181")', '=IMPORTRANGE("https://docs.google.com/spreadsheets/d/1ys-rIwopVtp_PDzr4tV_ycF1z1Vfug54BJ0IUVx1SNs","Średnia 7 dni SARS-CoV-2!I168:I181")']]\n\nrequest1 = service.spreadsheets().values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,\n
+                                                 range="Średnia 7 dni - Zakres 2 tygodnie!B3", valueInputOption="USER_ENTERED",\nbody={"values":RUN1}).execute()\n
 
+print(request1)''')
+f.close
 #inkrementacja
